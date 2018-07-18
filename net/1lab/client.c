6f073a678eab3e249033ae03cd711e6da693155f
ImNia <agnia-kimi1996@mail.ru>
2018-07-18 14:09:59 +0700

    Неет

--- net/1lab/client.c
+++ net/1lab/client.c
@@ -1,24 +1,29 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
 
 int main()
 {
-	struct sockaddr client_addr;
+	struct sockaddr_in client_addr;
 	char buf[10];
 
-	int sock_fd = socket(AF_LOCAL, SOCK_STREAM | SOCK_DGRAM, 0);
+	int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+	client_addr.sin_family = AF_INET;
+	client_addr.sin_port = htons(100);
+	client_addr.sin_addr.s_addr = htons(INADDR_LOOPBACK);
 	connect(sock_fd, (struct sockaddr*)&client_addr, sizeof(client_addr));
 //	recv(sock_fd, buf, sizeof(buf), NULL);
 	buf[0] = 'H';	
 	for(int i = 0; i < 10; i++)
 		printf("%c", buf[i]);
-		printf("\n");
-	send(sock_fd, buf, sizeof(buf), MSG_PEEK);
-	recv(sock_fd, buf, sizeof(buf), MSG_PEEK);
+	printf("\n");
+	send(sock_fd, buf, sizeof(buf), MSG_WAITALL);
+	recv(sock_fd, buf, sizeof(buf), MSG_WAITALL);
 	for(int i = 0; i < 10; i++)
 		printf("%c", buf[i]);
-		printf("\n");
+	printf("\n");
+	perror(NULL);
 	close(sock_fd);
 
 	return 0;
--- net/1lab/server.c
+++ net/1lab/server.c
@@ -1,21 +1,27 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
 
 int main()
 {
-	struct sockaddr serv_addr;
+	struct sockaddr_in serv_addr;
 	char buf[10];
-	int sock_fd = socket(AF_LOCAL, SOCK_STREAM | SOCK_DGRAM, 0);
+
+	int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(100);
+	serv_addr.sin_addr.s_addr = htons(INADDR_LOOPBACK);
 	bind(sock_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
 	listen(sock_fd, 5);	
 	accept(sock_fd, NULL, NULL);
-	recv(sock_fd, buf, sizeof(buf), MSG_PEEK);
+	recv(sock_fd, buf, sizeof(buf), MSG_WAITALL);
 	buf[1] = 'e';
 	for(int i = 0; i < 10; i++)
 		printf("%c", buf[i]);
 	printf("\n");
-	send(sock_fd, buf, sizeof(buf), MSG_PEEK);
+	send(sock_fd, buf, sizeof(buf), MSG_WAITALL);
+	perror(NULL);
 	close(sock_fd);
 
 	return 0;
